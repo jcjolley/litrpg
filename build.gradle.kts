0b209be23804fcaa7bb447906b9952c313fc1d6e
quarkusio <no-reply@quarkus.io>
2024-09-01 21:27:18 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+    kotlin("plugin.allopen") version "2.0.10"
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-amazon-services-bom:${quarkusPlatformVersion}"))
+    implementation("io.quarkiverse.amazonservices:quarkus-amazon-dynamodb-enhanced")
+    implementation("io.quarkus:quarkus-rest")
+    implementation("io.quarkus:quarkus-smallrye-openapi")
+    implementation("io.quarkus:quarkus-rest-jackson")
+    implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.4.7")
+    implementation("io.quarkus:quarkus-smallrye-fault-tolerance")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-websockets")
+    implementation("io.quarkus:quarkus-smallrye-health")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "org.jcjolley"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+allOpen {
+    annotation("jakarta.ws.rs.Path")
+    annotation("jakarta.enterprise.context.ApplicationScoped")
+    annotation("jakarta.persistence.Entity")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_21.toString()
+    kotlinOptions.javaParameters = true
+}
---
+++ src/native-test/kotlin/org/jcjolley/GreetingResourceIT.kt
@@ -0,0 +1,6 @@
+package org.jcjolley
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+
+@QuarkusIntegrationTest
+class GreetingResourceIT : GreetingResourceTest()
